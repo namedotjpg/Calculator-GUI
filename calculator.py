c3d503acd8922d3e957acafcfa051d20245fdae7
John Grohman <>
2021-01-24 23:53:55 -0600

    created canvas and frame for number inputs

--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 
-canvas_width = 700
-canvas_height = 700
-
 root = tk.Tk()
 
+canvas = tk.Canvas(root, height = 410, width = 310, bg = 'black')
+canvas.pack()
 
+number_frame = tk.Frame(root, bg = 'red')
+number_frame.place(relwidth = .98, relheight = .15, relx = .01, rely = .01)
 
 root.mainloop()
